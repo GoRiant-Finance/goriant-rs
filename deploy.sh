b343fb846af0058f78c074b04e351ed2386ba95a
lamle <lam.le@mservice.com.vn>
2021-05-16 14:19:50 +0700

    improve deploy.sh: grep programId from output and update config.json

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
-anchor build && anchor deploy
+#!/bin/bash
+
+anchor build && anchor deploy | awk '/Program Id:/ {print substr($0,13,44)}' > programId.txt
+
+program_id=$( cat programId.txt )
+
+rm programId.txt
+
+echo "{\"programId\":\"${program_id}\"}" > config.json
