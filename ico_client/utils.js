fa555be557087aba9d5daad3b8060a91f2ff42e2
lamle <lam.le@mservice.com.vn>
2021-05-30 12:08:28 +0700

    init Riant ICO program

---
+++ ico_client/utils.js
@@ -0,0 +1,146 @@
+const anchor = require("@project-serum/anchor");
+const serumCmn = require("@project-serum/common");
+const fs = require('fs');
+
+const provider = anchor.Provider.local('http://127.0.0.1:8899');
+async function log_state(state) {
+  console.log("state.key: ", state.key.toString());
+  console.log("state.authority: ", state.authority.toString());
+  console.log("state.imprint: ", state.imprint.toString());
+  console.log("state.nonce: ", state.nonce.toString())
+  console.log("state.withdrawTimeLock: ", state.withdrawTimeLock.toString())
+  console.log("state.mint: ", state.mint.toString())
+  console.log("state.poolMint: ", state.poolMint.toString())
+  console.log("state.poolMintDecimal", state.poolMintDecimal.toString())
+  console.log("state.precisionFactor: ", state.precisionFactor.toString())
+  console.log("state.stakeRate: ", state.stakeRate.toString())
+  console.log("state.accTokenPerShare: ", state.accTokenPerShare.toString())
+  console.log("state.startBlock: ", state.startBlock.toString())
+  console.log("state.bonusEndBlock: ", state.bonusEndBlock.toString())
+  console.log("state.lastRewardBlock: ", state.lastRewardBlock.toString())
+  console.log("state.rewardPerBlock: ", state.rewardPerBlock.toString())
+  console.log("state.rewardVault: ", state.rewardVault.toString(), " - amount: ", await tokenBalance(state.rewardVault))
+}
+async function createBalanceSandbox(provider, r, registrySigner) {
+  const spt = new anchor.web3.Account();
+  const vaultStake = new anchor.web3.Account();
+  const vaultPw = new anchor.web3.Account();
+
+  const lamports = await provider.connection.getMinimumBalanceForRentExemption(
+    165
+  );
+
+  const createSptIx = await serumCmn.createTokenAccountInstrs(
+    provider,
+    spt.publicKey,
+    r.poolMint,
+    registrySigner,
+    lamports
+  );
+  const createVaultStakeIx = await serumCmn.createTokenAccountInstrs(
+    provider,
+    vaultStake.publicKey,
+    r.mint,
+    registrySigner,
+    lamports
+  );
+  const createVaultPwIx = await serumCmn.createTokenAccountInstrs(
+    provider,
+      vaultPw.publicKey,
+    r.mint,
+    registrySigner,
+    lamports
+  );
+  let tx0 = new anchor.web3.Transaction();
+  tx0.add(
+    ...createSptIx,
+    ...createVaultStakeIx,
+    ...createVaultPwIx
+  );
+  let signers0 = [spt, vaultStake, vaultPw];
+
+  const tx = {tx: tx0, signers: signers0};
+
+  return [
+    tx,
+    {
+      spt: spt.publicKey,
+      vaultStake: vaultStake.publicKey,
+      vaultPw: vaultPw.publicKey,
+    },
+  ];
+}
+
+async function balance(address) {
+  return provider.connection.getBalance(address);
+}
+
+async function printMemberAccountInfo(memberAccount) {
+  console.log("memberAccount.authority: ", memberAccount.authority.toString())
+  console.log("memberAccount.metadata: ", memberAccount.metadata.toString())
+  console.log("memberAccount.nonce: ", memberAccount.nonce.toString())
+  console.log("memberAccount.rewardDebt: ", memberAccount.rewardDebt.toString())
+
+  let memberBalances = memberAccount.balances;
+
+  console.log("memberAccount.balances");
+  console.log("spt: ", memberBalances.spt.toString(), " - amount: ", await tokenBalance(memberBalances.spt))
+  console.log("vaultStake: ", memberBalances.vaultStake.toString(), " - amount: ", await tokenBalance(memberBalances.vaultStake))
+  console.log("vaultPw: ", memberBalances.vaultPw.toString(), " - amount: ", await tokenBalance(memberBalances.vaultPw))
+
+}
+
+
+async function printStructInfo(name, v) {
+  console.log(" ", name);
+  console.log("   mint: ", v.mint.toBase58(), " ", await balance(v.mint));
+  console.log("   owner: ", v.owner.toBase58(), " ", await balance(v.owner));
+  console.log("   amount: ", v.amount.toString());
+  console.log("   delegated amount: ", v.delegatedAmount.toString());
+}
+
+async function printBalance(name, v) {
+  console.log("    ", name);
+  console.log("        spt: ", v.spt.toBase58(), " ", await balance(v.spt));
+  console.log("        vault: ", v.vault.toBase58(), " ", await balance(v.vault));
+  console.log("        vaultStake: ", v.vaultStake.toBase58(), " ", await balance(v.vaultStake));
+  console.log("        vaultPendingWithdraw: ", v.vaultPendingWithdraw.toBase58(), " ", await balance(v.vaultPendingWithdraw));
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function tokenBalance(address) {
+  return (await provider.connection
+    .getTokenAccountBalance(await new anchor.web3.PublicKey(address))).value;
+}
+
+function writeConfig(c) {
+  let data = JSON.stringify(c);
+  fs.writeFileSync('./config.json', data);
+}
+
+function readConfig() {
+  return JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+}
+function readIdl() {
+  return JSON.parse(fs.readFileSync('../target/idl/ico.json', 'utf8'));
+
+}
+module.exports = {
+  provider,
+  readIdl,
+  tokenBalance,
+  sleep,
+  createBalanceSandbox,
+  log_state,
+  balance,
+  printBalance,
+  printStructInfo,
+  printMemberAccountInfo,
+  readConfig,
+  writeConfig
+};
--- programs/ico/src/lib.rs
+++ programs/ico/src/lib.rs
@@ -1,10 +1,16 @@
-use anchor_lang::prelude::*;
+mod error;
+
+use {
+    anchor_lang::prelude::*,
+    anchor_spl::token::{self, TokenAccount},
+    std::convert::Into,
+};
+
+use error::ErrorCode;
 
 #[program]
 pub mod ico {
     use super::*;
-
-    /// u16 : MAX 65,536
 
     #[state]
     pub struct RiantICO {
@@ -16,24 +22,82 @@ pub mod ico {
         pub start: i64,
         /// Capset in SOL
         pub cap: u16,
+        /// Total raised in SOL
+        pub raised_amount: u16,
         /// Number of tokens per SOL
         pub rate: u16,
-        /// Initial number of tokens available
-        pub initial_tokens: u8,
         /// Privileged account.
         pub owner: Pubkey,
         /// Beneficiary account: Account for receiving SOL
         pub beneficiary: Pubkey,
-        /// Token vault for distribution
-        pub pool_mint: Pubkey,
-        /// Total raised in SOL
-        pub raised_amount: u8
+        /// ICO pool token for distribution
+        pub ico_pool: Pubkey,
     }
 
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    impl RiantICO {
+        pub fn new(ctx: Context<InitializeRiantIco>,
+                   state_pub_key: Pubkey,
+                   start: i64,
+                   cap: u16,
+                   rate: u16,
+        ) -> Result<Self, ProgramError>
+        {
+            msg!("Init Riant ICO");
+            msg!("Init Riant ICO new program");
+
+            msg!("Transfer 200_000 RIANT from creator to ICO pool vault");
+            msg!("Cap {} - Rate {}", cap, rate);
+            let cpi_ctx = CpiContext::new(
+                ctx.accounts.token_program.clone(),
+                token::Transfer {
+                    from: ctx.accounts.depositor.to_account_info(),
+                    to: ctx.accounts.ico_pool.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            );
+            let amount = (cap as u64)
+                .checked_mul(rate as u64).unwrap()
+                .checked_mul(1000000000).unwrap() ;
+            if let Ok(()) = token::transfer(cpi_ctx, amount) {} else {
+                return Err(ErrorCode::TransferTokenFail.into());
+            };
+
+            let riant_ico = Self {
+                key: state_pub_key,
+                owner: *ctx.accounts.authority.key,
+                beneficiary: *ctx.accounts.authority.key,
+                initialized: true,
+                start,
+                cap,
+                rate,
+                ico_pool: *ctx.accounts.ico_pool.to_account_info().key,
+                raised_amount: 0,
+            };
+
+            Ok(riant_ico)
+        }
+    }
+
+    pub fn buy(_ctx: Context<PurchaseRequest>, _amount: u32) -> Result<(), ProgramError> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct PurchaseRequest<'info> {
+    #[account(signer)]
+    authority: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeRiantIco<'info> {
+    #[account(signer)]
+    authority: AccountInfo<'info>,
+    #[account(mut)]
+    depositor: AccountInfo<'info>,
+    #[account(mut)]
+    ico_pool: CpiAccount<'info, TokenAccount>,
+    #[account("token_program.key == &token::ID")]
+    token_program: AccountInfo<'info>,
+    rent: Sysvar<'info, Rent>,
+}
